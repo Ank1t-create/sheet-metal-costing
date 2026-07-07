02a9e96cbbc25674396440e40007de28605f7e79
Ankit Kumar Mishra <ankitkumarmishra020503@gmail.com>
2026-07-07 18:09:32 +0530

    Added standard sheet size selection

--- app.py
+++ app.py
@@ -19,22 +19,34 @@ def calculator():
     rmc = 0
 
     material = ""
+    sheet_size = ""
 
     thickness = ""
-    length = ""
-    width = ""
     blanks_per_sheet = ""
     part_weight = ""
     rm_rate = ""
     scrap_rate = ""
 
+    length = 0
+    width = 0
+
     if request.method == "POST":
 
         material = request.form["material"]
+        sheet_size = request.form["sheet_size"]
 
         thickness = float(request.form["thickness"])
-        length = float(request.form["length"])
-        width = float(request.form["width"])
+
+        # Standard Sheet Sizes
+        sheet_sizes = {
+            "2500x1250": (2500, 1250),
+            "2440x1220": (2440, 1220),
+            "3000x1500": (3000, 1500),
+            "2000x1000": (2000, 1000),
+            "1500x1250": (1500, 1250)
+        }
+
+        length, width = sheet_sizes[sheet_size]
 
         blanks_per_sheet = float(request.form["blanks_per_sheet"])
         part_weight = float(request.form["part_weight"])
@@ -42,7 +54,7 @@ def calculator():
         rm_rate = float(request.form["rm_rate"])
         scrap_rate = float(request.form["scrap_rate"])
 
-        # Select density
+        # Material Density
         if material == "CR":
             density = 7.85
         elif material == "HR":
@@ -52,18 +64,18 @@ def calculator():
         else:
             density = 7.85
 
-        # Sheet weight
+        # Sheet Weight
         weight = round(sheet_weight(thickness, length, width, density), 3)
 
-        # Input weight per part
+        # Input Weight
         input_weight = round(weight / blanks_per_sheet, 3)
 
-        # Scrap weight
+        # Scrap Weight
         scrap_weight = round(input_weight - part_weight, 3)
 
-        # Cost calculations
-        rm_cost = input_weight * rm_rate
-        scrap_cost = scrap_weight * scrap_rate
+        # Cost Calculations
+        rm_cost = round(input_weight * rm_rate, 2)
+        scrap_cost = round(scrap_weight * scrap_rate, 2)
 
         # Final RMC
         rmc = round(rm_cost - scrap_cost, 2)
@@ -71,6 +83,7 @@ def calculator():
     return render_template(
         "calculator.html",
         material=material,
+        sheet_size=sheet_size,
         weight=weight,
         input_weight=input_weight,
         scrap_weight=scrap_weight,
@@ -78,12 +91,12 @@ def calculator():
         scrap_cost=scrap_cost,
         rmc=rmc,
         thickness=thickness,
-        length=length,
-        width=width,
         blanks_per_sheet=blanks_per_sheet,
         part_weight=part_weight,
         rm_rate=rm_rate,
         scrap_rate=scrap_rate
     )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
